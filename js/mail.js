c6713d42032a7e7af6de8c536b48a3d96a776262
LuisLunaV <luis.a.luna.v@outlook.com>
2022-06-20 21:50:14 -0500

    Se agrega la ventana 'aviso de mensaje enviado'

--- js/mail.js
+++ js/mail.js
@@ -1,18 +1,25 @@
 import { seccionContacto } from "./ventana-modal.js";
-const inputNombre = document.querySelector("#nombre"),
-  inputCorreo = document.querySelector("#correo"),
-  txtComentario = document.querySelector("#comentario");
+const inputNombre   = document.querySelector("#nombre"),
+      inputCorreo   = document.querySelector("#correo"),
+      txtComentario = document.querySelector("#comentario"),
+      contenedorModal = document.querySelector('.contenedor-modal');
 
 export const enviarCorreo = () => {
+
   seccionContacto.addEventListener("click", (event) => {
-    // event.preventDefault();
-    const evnviar = event.target.value;
-    if (evnviar == "Enviar") {
+
+    const enviar = event.target.value;
+    
+    if ( enviar == "Enviar") {
+
       if (
-        inputNombre.value != "" &&
-        inputCorreo.value != "" &&
+        inputNombre.value   != "" &&
+        inputCorreo.value   != "" &&
         txtComentario.value != ""
       ) {
+
+
+        // Conecta con la API email.js y envia la informacion a su destino
         const btn = document.getElementById("button");
 
         document
@@ -36,12 +43,50 @@ export const enviarCorreo = () => {
             );
           });
 
+        // Limpiamos las cajas y quitamos los elementos del form.
         setTimeout(() => {
-          inputNombre.value = "";
-          inputCorreo.value = "";
+
+          inputNombre.value   = "";
+          inputCorreo.value   = "";
           txtComentario.value = "";
+          do{
+
+            contenedorModal.removeChild( contenedorModal.firstElementChild);
+            console.log('borrado')
+          }while(contenedorModal.firstElementChild)
+
+
         }, 1000);
+
+        // Ejecutamos la funcion de mensaje de enviado
+        setTimeout(()=>{
+          mensajeDeEnviado();
+        },1500)
+        
+        //Recargamos la pagina
+        setTimeout(()=>{
+          location.reload()
+        },6000)
+
       } else console.log("completa los campos");
     }
   });
+};
+
+
+//Agregamos la imagen lottie al contenedor modal.
+//Aviso visual de mensaje enviado
+
+const mensajeDeEnviado=()=>{
+
+  const html = `    <lottie-player
+  src="https://assets1.lottiefiles.com/packages/lf20_2njvpkre.json"
+  background="transparent"
+  speed="1"
+  autoplay
+></lottie-player>
+`;
+
+contenedorModal.innerHTML += html;
+
 };
