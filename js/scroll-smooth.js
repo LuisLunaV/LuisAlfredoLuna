aea44ffbc3abee255eaa9298fde1597479df3383
LuisLunaV <luis.a.luna.v@outlook.com>
2022-08-26 04:39:29 -0500

    Se modifica el archivo scroll-month

--- js/scroll-smooth.js
+++ js/scroll-smooth.js
@@ -1,34 +1,43 @@
 window.onload = () => {
-    let links = document.querySelectorAll(".link");
-  
-    let inicio = links[0];
+
+    const linkRespomsive = document.querySelectorAll(".link");
+    const linkMenu = document.querySelectorAll(".link-menu");
+
+    linksMenu( linkRespomsive );
+    linksMenu( linkMenu );
+
+  };
+
+  const linksMenu =( link )=>{
+
+    let inicio = link[0];
     inicio.addEventListener("click", () => {
       scrollSuave("#inicio", 1000, 0);
     });
   
-    let informacion = links[1];
+    let informacion = link[1];
     informacion.addEventListener("click", () => {
       scrollSuave("#informacion", 900, 0);
     });
   
-    let tecnologias = links[2];
+    let tecnologias = link[2];
     tecnologias.addEventListener("click", () => {
       scrollSuave("#tecnologias", 900, 0);
     });
-    let proyectos = links[3];
+    let proyectos = link[3];
     proyectos.addEventListener("click", () => {
       scrollSuave("#proyectos", 900, 0);
     });
-    let servicios = links[4];
+    let servicios = link[4];
     servicios.addEventListener("click", () => {
       scrollSuave("#servicios", 900, 0);
     });
-    let contacto = links[5];
+    let contacto = link[5];
     contacto.addEventListener("click", () => {
       scrollSuave("#contacto", 1000, 0);
     });
+
   };
-  
   const scrollSuave = (objetivo, duracion, compensacion) => {
     let elemObj = document.querySelector(objetivo);
     let elemPos = elemObj.getBoundingClientRect().top - compensacion;
