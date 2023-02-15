7befab01b7ccc2f0b79d5c2aac16bc689bfab56a
LuisLunaV <luis.a.luna.v@outlook.com>
2023-02-15 02:49:51 -0600

    Se refactoriza los eventos en funciones

--- js/ventana-modal.js
+++ js/ventana-modal.js
@@ -1,37 +1,25 @@
-const seccionContacto           = document.querySelector('.mi-contacto'),
-      form                      = document.querySelector('.formulario'),
-      cerrar                    = document.querySelector('.btn-cerrar'),
-      ventanaDelFormulario      = document.querySelector('.contenedor-modal');
+const seccionContacto = document.querySelector(".mi-contacto"),
+  form = document.querySelector(".formulario"),
+  cerrar = document.querySelector(".btn-cerrar"),
+  ventanaDelFormulario = document.querySelector(".contenedor-modal");
 
-const ventanaModal =()=>{
+const ventanaModal = () => {
+  seccionContacto.addEventListener("click", (event) => {
+    const btnMensaje = event.target.text;
 
-    seccionContacto.addEventListener('click', ( event )=>{
-       
-        const btnMensaje = event.target.text;
- 
+    if (btnMensaje.trim() === "Mensaje") {
+      ventanaDelFormulario.classList.remove("ocultar");
+      ventanaDelFormulario.classList.add("mostrar");
 
-            if(btnMensaje.trim() === 'Mensaje'){
-                ventanaDelFormulario.classList.remove('ocultar');
-                ventanaDelFormulario.classList.add('mostrar');
+      form.classList.add("tranformY-0");
+    }
+  });
 
-                form.classList.add('tranformY-0');
-            }
-                
-});
-
-cerrar.addEventListener('click',()=>{
-    ventanaDelFormulario.classList.remove('mostrar');
-    ventanaDelFormulario.classList.add('ocultar');
-    form.classList.remove('tranformY-0');
-
-    });
-
+  cerrar.addEventListener("click", () => {
+    ventanaDelFormulario.classList.remove("mostrar");
+    ventanaDelFormulario.classList.add("ocultar");
+    form.classList.remove("tranformY-0");
+  });
 };
 
-
-
-export{
-    ventanaModal,
-    seccionContacto
-
-}
+export { ventanaModal, seccionContacto };
